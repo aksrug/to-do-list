8d20120654a0ec9faf370c692347b09a481ba9d4
FIRSTNAME LASTNAME <YOUR@EMAIL.COM>
2024-08-22 11:44:54 +0300

    update

--- src/components/toDoList.jsx
+++ src/components/toDoList.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 
 const ToDoList = () => {
     
-    const [tasks, setTasks] = useState([])
+    const [tasks, setTasks] = useState(["Išplauti indus", "Pasidaryti valgyti", "Nenumirti"])
     const [newTask, setNewTask] = useState("")
 
     function handleInputChange(event) {
-
+        console.log(event.target.value);
+        setNewTask(event.target.value);
     }
 
     function addTask() {
+
 
     }
 
@@ -31,9 +33,25 @@ const ToDoList = () => {
     
     <h1>TO-DO-List</h1>
     <div>
-        <input type="text" placeholder="Enter A task" value={newTask} onChange={} />
+        <input type="text" placeholder="Enter A task" value={newTask} onChange={handleInputChange} />
+        <button className="add-button" onClick={addTask}>ADD</button>
     </div>
+
+    <ul>
+        {
+            tasks.map((task,index) =>
+            <li key={index} >
+                <span className="text">{task}</span>
+                <button className="delete-button" onClick={()=>deleteTask(index)}>Delete</button>
+                <button className="move-button" onClick={()=>moveTaskUp(index)}>moveUp</button>
+                <button className="move-button" onClick={()=>moveTaskDown(index)}>moveDown</button>
+            </li>
+            )
+        }
+    </ul>
     
+
+
     </>
 }
 
